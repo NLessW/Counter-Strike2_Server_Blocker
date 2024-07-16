b43e1b63549bd16b2b5fc0513daaef04abccda86
Less <63160418+NLessW@users.noreply.github.com>
2024-07-16 18:33:58 +0900

    Update Asia_Server_Blocker_Theme.py

--- src/Asia_Server_Blocker_Theme.py
+++ src/Asia_Server_Blocker_Theme.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import font, messagebox, ttk
 import subprocess
 import re
+import sys
+import os
 
 server_ips = {
     "Seoul": "146.66.152.1",
@@ -11,9 +13,25 @@ server_ips = {
     "Singapore": "103.28.54.1"
 }
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
 def ping_server(ip):
     try:
-        output = subprocess.check_output(["ping", "-n", "1", "-w", "1000", ip], universal_newlines=True)
+        startupinfo = None
+        if sys.platform == "win32":
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        output = subprocess.check_output(["ping", "-n", "1", "-w", "1000", ip], 
+                                         universal_newlines=True, 
+                                         startupinfo=startupinfo,
+                                         creationflags=subprocess.CREATE_NO_WINDOW)
         match = re.search(r"시간=(\d+)ms", output)
         if match:
             return int(match.group(1))
@@ -33,7 +51,7 @@ def run_ping_test():
 def check_sdr_rule_exists():
     command = "netsh advfirewall firewall show rule name=blockSDR"
     try:
-        output = subprocess.check_output(command, shell=True, universal_newlines=True)
+        output = subprocess.check_output(command, shell=True, universal_newlines=True, creationflags=subprocess.CREATE_NO_WINDOW)
         return "No rules match the specified criteria." not in output
     except subprocess.CalledProcessError:
         return False
@@ -42,7 +60,7 @@ def add_sdr_rule():
     if not check_sdr_rule_exists():
         command = "netsh advfirewall firewall add rule name=blockSDR dir=out action=block profile=any protocol=any remoteip=143.137.146.0-143.137.146.255"
         try:
-            subprocess.run(command, shell=True, check=True)
+            subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
             print("SDR rule added successfully.")
         except subprocess.CalledProcessError:
             print("Failed to add SDR rule.")
@@ -52,56 +70,56 @@ def toggle_server_block(server):
         if server == "Seoul":
             if checkKorea.get() == 1:
                 command = "netsh advfirewall firewall add rule name=blockSeoul dir=out action=block profile=any protocol=any remoteip=146.66.152.0-146.66.152.255"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "Seoul server blocked successfully!")
                 status_labels["Seoul"].config(text="● ", fg='#39FF14')
             else:
                 command = "netsh advfirewall firewall delete rule name=blockSeoul"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "Seoul server block removed!")
                 status_labels["Seoul"].config(text="● ", fg='red')
         elif server == "Tokyo":
             if checkJapan.get() == 1:
                 command = "netsh advfirewall firewall add rule name=blockTokyo dir=out action=block profile=any protocol=any remoteip=45.121.184.0-45.121.184.255,45.121.186.0-45.121.186.255,45.121.187.0-45.121.187.255,61.14.157.0-61.14.157.255,146.66.152.0-146.66.152.255,155.133.239.0-155.133.239.255,155.133.245.0-155.133.245.255"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "Tokyo server blocked successfully!")
                 status_labels["Tokyo"].config(text="● ", fg='#39FF14')
             else:
                 command = "netsh advfirewall firewall delete rule name=blockTokyo"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "Tokyo server block removed!")
                 status_labels["Tokyo"].config(text="● ", fg='red')
         elif server == "HongKong":
             if checkHongkong.get() == 1:
                 command = "netsh advfirewall firewall add rule name=blockHongKong dir=out action=block profile=any protocol=any remoteip=103.28.54.0-103.28.54.255,155.133.244.0-155.133.244.255,153.254.86.0-153.254.86.255"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "HongKong server blocked successfully!")
                 status_labels["HongKong"].config(text="● ", fg='#39FF14')
             else:
                 command = "netsh advfirewall firewall delete rule name=blockHongKong"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "HongKong server block removed!")
                 status_labels["HongKong"].config(text="● ", fg='red')
         elif server == "India":
             if checkIndia.get() == 1:
                 command = "netsh advfirewall firewall add rule name=blockIndia dir=out action=block profile=any protocol=any remoteip=10.130.205.0-10.130.205.255,45.113.191.0-45.113.191.255,116.202.224.0-116.202.224.255,155.133.232.0-155.133.232.255,155.133.233.0-155.133.233.255,180.149.41.0-180.149.41.255,182.79.252.0-182.79.252.255"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "India server blocked successfully!")
                 status_labels["India"].config(text="● ", fg='#39FF14')
             else:
                 command = "netsh advfirewall firewall delete rule name=blockIndia"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "India server block removed!")
                 status_labels["India"].config(text="● ", fg='red')
         elif server == "Singapore":
             if checkAsia.get() == 1:
                 command = "netsh advfirewall firewall add rule name=blockSingapore dir=out action=block profile=any protocol=any remoteip=45.121.184.0-45.121.184.255,45.121.185.0-45.121.185.255,10.156.7.0-10.156.7.255,103.28.54.0-103.28.54.255,103.28.55.0-103.28.55.255,103.10.124.0-103.10.124.255"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "Singapore server blocked successfully!")
                 status_labels["Singapore"].config(text="● ", fg='#39FF14')
             else:
                 command = "netsh advfirewall firewall delete rule name=blockSingapore"
-                subprocess.run(command, shell=True, check=True)
+                subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
                 messagebox.showinfo("Success", "Singapore server block removed!")
                 status_labels["Singapore"].config(text="● ", fg='red')
 
@@ -124,7 +142,7 @@ def reset_all_blocks():
     success = True
     for command in commands:
         try:
-            subprocess.run(command, shell=True, check=True)
+            subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
         except subprocess.CalledProcessError:
             success = False
     
@@ -154,7 +172,7 @@ def all_blocks():
     success = True
     for command in commands:
         try:
-            subprocess.run(command, shell=True, check=True)
+            subprocess.run(command, shell=True, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
         except subprocess.CalledProcessError:
             success = False
             
@@ -178,9 +196,11 @@ def all_blocks():
 window = tk.Tk()
 
 window.title("CS2 Server Blocker")
-window.iconbitmap('cs2.ico')
+icon_path = resource_path('cs2.ico')
+window.iconbitmap(icon_path)
 window.geometry("300x450")
 window.configure(bg='#1E1E1E')
+window.resizable(False, False)  # 창 크기 조정 불가능하게 설정
 
 style = ttk.Style()
 style.theme_use('clam')
